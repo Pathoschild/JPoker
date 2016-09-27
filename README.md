2d4a66a2e9c955d144d4eb3327b2ce51ee0d1872
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-09-27 11:23:39 -0400

    add project structure + readme

---
+++ README.md
@@ -0,0 +1,2 @@
+**JPoker** is a simple implementation of Texas Hold 'em (head-to-head tournament style) to teach
+myself cards and Java.
---
+++ src/com/jplamondonw/jpoker/Main.java
@@ -0,0 +1,14 @@
+package com.jplamondonw.jpoker;
+
+/**
+ * The entry point for the poker game.
+ */
+public class Main {
+    /**
+     * The entry point for the poker game.
+      * @param args The command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
