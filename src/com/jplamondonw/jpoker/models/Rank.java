d5be5824fba3065e1bace6a86953bb67dc2342f2
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-09-27 16:07:32 -0400

    add rank values

--- src/com/jplamondonw/jpoker/models/Rank.java
+++ src/com/jplamondonw/jpoker/models/Rank.java
@@ -6,23 +6,28 @@ package com.jplamondonw.jpoker.models;
 public enum Rank {
     // Constants
     //******************************
-    A     ("A",  "A    \n     \n  %  \n     \n    A"),
-    TWO   ("2",  "2    \n  %  \n     \n  %  \n    2"),
-    THREE ("3",  "3    \n  %  \n  %  \n  %  \n    3"),
-    FOUR  ("4",  "4    \n%   %\n     \n%   %\n    4"),
-    FIVE  ("5",  "5    \n%   %\n  %  \n%   %\n    5"),
-    SIX   ("6",  "6    \n%   %\n%   %\n%   %\n    6"),
-    SEVEN ("7",  "7    \n%   %\n% % %\n%   %\n    7"),
-    EIGHT ("8",  "8    \n% % %\n%   %\n% % %\n    8"),
-    NINE  ("9",  "9    \n% % %\n% % %\n% % %\n    9"),
-    TEN   ("10", "10  %\n% % %\n% % %\n% % %\n   10"),
-    JACK  ("J",  "J    \n     \nJACK \n     \n    J"),
-    QUEEN ("Q",  "Q    \n     \nQUEEN\n     \n    Q"),
-    KING  ("K",  "K    \n     \nKING \n     \n    K");
+    TWO   (2,  "2",  "2    \n  %  \n     \n  %  \n    2"),
+    THREE (3,  "3",  "3    \n  %  \n  %  \n  %  \n    3"),
+    FOUR  (4,  "4",  "4    \n%   %\n     \n%   %\n    4"),
+    FIVE  (5,  "5",  "5    \n%   %\n  %  \n%   %\n    5"),
+    SIX   (6,  "6",  "6    \n%   %\n%   %\n%   %\n    6"),
+    SEVEN (7,  "7",  "7    \n%   %\n% % %\n%   %\n    7"),
+    EIGHT (8,  "8",  "8    \n% % %\n%   %\n% % %\n    8"),
+    NINE  (9,  "9",  "9    \n% % %\n% % %\n% % %\n    9"),
+    TEN   (10, "10", "10  %\n% % %\n% % %\n% % %\n   10"),
+    JACK  (11, "J",  "J    \n     \nJACK \n     \n    J"),
+    QUEEN (12, "Q",  "Q    \n     \nQUEEN\n     \n    Q"),
+    KING  (13, "K",  "K    \n     \nKING \n     \n    K"),
+    A     (14, "A",  "A    \n     \n  %  \n     \n    A");
 
 
     // Properties
     //******************************
+    /**
+     * The card value when choosing a high card.
+     */
+    public final int value;
+
     /**
      * The display symbol.
      */
@@ -38,10 +43,12 @@ public enum Rank {
     //******************************
     /**
      * Construct an instance.
+     * @param value The card value when choosing a high card.
      * @param symbol The display symbol.
      * @param layout The card layout on a five-by-five grid, where % represents the suit symbol.
      */
-    Rank(String symbol, String layout) {
+    Rank(int value, String symbol, String layout) {
+        this.value = value;
         this.symbol = symbol;
         this.layout = layout;
     }
